968078661374bd43feaef3af9deee8e34f2ed537
Kishan Kumar Seth <kkseth412@gmail.com>
2022-05-22 23:08:42 +0530

    28

---
+++ 28. Implement strStr().cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int i=0,j=0;
+        while(i<haystack.size())
+        {
+            if(haystack[i]==needle[j])
+        }
+    }
+};
