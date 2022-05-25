7c746c22006f9bdc781bbd67b0007229cac49d6a
Kishan Kumar Seth <kkseth412@gmail.com>
2022-05-25 18:38:01 +0530

    628

---
+++ 628. Maximum Product of Three Numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int a=INT_MIN, b=INT_MIN, c=INT_MIN, d=INT_MAX, e=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>a)
+            {
+                c=b;
+                b=a;
+                a=nums[i];
+            }
+            else if(nums[i]>b)
+            {
+                c=b;
+                b=nums[i];
+            }
+            else if(c<nums[i])
+                c=nums[i];
+            if(nums[i]<d)
+            {
+                e=d;
+                d=nums[i];
+            }
+            else if(nums[i]<e)
+                e=nums[i];
+        }
+        return max(a*b*c,a*d*e);
+    }
+};
